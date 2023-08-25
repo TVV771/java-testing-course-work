b2ef945134e7ab3fab7c60b19759a32d24c20289
TV#BJJ <tv041000@gmail.com>
2023-08-25 17:51:50 +1000

    .

---
+++ src/test/java/com/skypro/simplebanking/SimpleBankingApplicationTests.java
@@ -0,0 +1,12 @@
+package com.skypro.simplebanking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleBankingApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+
+}
